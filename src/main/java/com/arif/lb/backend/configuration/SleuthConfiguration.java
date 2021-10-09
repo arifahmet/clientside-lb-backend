15a41fe418204f128ee172b3d38c8bc3e7fc34fb
Arif Ahmet <arifahmetbarbak@gmail.com>
2021-10-10 01:09:11 +0300

    add zipkin trace

---
+++ src/main/java/com/arif/lb/backend/configuration/SleuthConfiguration.java
@@ -0,0 +1,49 @@
+package com.arif.lb.backend.configuration;
+
+import brave.handler.MutableSpan;
+import brave.handler.SpanHandler;
+import brave.propagation.TraceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Configuration
+public class SleuthConfiguration {
+
+    @Bean
+    SpanHandler handlerOne() {
+        return new SpanHandler() {
+            @Override
+            public boolean end(TraceContext traceContext, MutableSpan span,
+                               Cause cause) {
+                span.localIp(getIp());
+                span.remoteIp(getIp());
+                span.localServiceName(getInfo());
+                return true; // keep this span
+            }
+
+            private String getInfo() {
+                String res = "lb-backend-deployment";
+                try {
+                    InetAddress inetAddress = InetAddress.getLocalHost();
+                    res = inetAddress.getHostName();
+                } catch (UnknownHostException ignored) {
+                }
+                return res;
+            }
+
+            private String getIp() {
+                String res = "lb-backend-deployment";
+                try {
+                    InetAddress inetAddress = InetAddress.getLocalHost();
+                    res = inetAddress.getHostAddress();
+                } catch (UnknownHostException ignored) {
+                }
+                return res;
+            }
+        };
+    }
+}
--- src/main/java/com/arif/lb/backend/controller/HelloController.java
+++ src/main/java/com/arif/lb/backend/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.arif.lb.backend.controller;
 
 
 import com.arif.lb.backend.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/hello")
+@Slf4j
 public class HelloController {
     private final HelloService helloService;
 
@@ -20,6 +22,7 @@ public class HelloController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> hello(@RequestParam(required = false) String name) {
+        log.info("Hello controller took name: " + name);
         return ResponseEntity.ok(helloService.hello(name));
     }
 }
